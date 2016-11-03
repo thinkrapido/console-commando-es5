bbfc091d13af9606774f9270bb19e4684155dedc
Romeo Disca <romeo.disca@gmail.com>
2016-11-03 10:12:30 +0100

    added polyfill for String.prototype.repeat

--- src/formatter.ts
+++ src/formatter.ts
@@ -5,6 +5,51 @@ import * as Immutable from 'immutable'
 import debug     from './debug'
 
 
+// polyfill
+function repeat(count) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count != count) {
+      count = 0;
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+      if ((count & 1) == 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    // Could we try:
+    // return Array(count + 1).join(this);
+    return rpt;
+  }
+
+// polyfill end
+
 export interface Formatter {
   padCommand: () => (options: any) => string
   padSubCommand: () => (options: any) => string
@@ -68,7 +113,7 @@ const pad = ({
  */
 const stringPad = (text: string, amount: number, direction: Direction, character: string) => {
   const chunkLen = amount - text.length
-  const chunk = chunkLen > 0 ? character.repeat(chunkLen) : ''
+  const chunk = chunkLen > 0 ? repeat.call(character, chunkLen) : ''
   const pre = direction === Direction.LEFT ? chunk : ''
   const pos = direction !== Direction.LEFT ? chunk : ''
   return `${pre}${text}${pos}`
